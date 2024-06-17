a97e16424f246b61929b1eca290dc286bd3d5a35
youssefrayenfehmi <yousseffehmi99@gmail.com>
2024-06-17 14:42:04 +0100

    first

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+// Navbar.js
+
+import React, { useState } from 'react';
 import './Navbar.css';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import { Link } from 'react-router-dom';
+
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <a href="/" className="navbar-logo">MathildaLearning</a>
-        <ul className="nav-menu">
-          <li className="nav-item"><a href="/formation" className="nav-link">Foramtion IA</a></li>
+        <button className="navbar-toggle-btn" onClick={toggleMenu}>
+          Menu
+        </button>
+        <ul className={`nav-menu ${showMenu ? 'active' : ''}`}>
+          <li className="nav-item"><a href="/formation" className="nav-link">Formation IA</a></li>
           <li className="nav-item"><a href="/audit" className="nav-link">Audit & Conseil</a></li>
-          <li className="nav-item"><a href="/cof" className="nav-link">Confèrence</a></li>
-          <li className="nav-item"><Link to="/cale" target="_blank" className="nav-link">Prendre rendez-vous{<CalendarMonthIcon></CalendarMonthIcon>}</Link></li>
+          <li className="nav-item"><a href="/conference" className="nav-link">Conférence</a></li>
+          <li className="nav-item">
+            <Link to="/appointment" className="nav-link" target="_blank">
+              Prendre rendez-vous <CalendarMonthIcon />
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
