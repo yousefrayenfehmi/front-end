fb5e4b02ab0ab4c7ad370c4109b9b2447101145e
youssefrayenfehmi <yousseffehmi99@gmail.com>
2024-06-18 12:21:05 +0100

    first

--- src/formationIA/firstshow.jsx
+++ src/formationIA/firstshow.jsx
@@ -1,38 +1,33 @@
-// src/HomePage.js
 import React from 'react';
-import './firstshow.css'; // We'll create this CSS file next
-import backgroundImage from '../../public/img/formation.jpg'; // Assurez-vous que l'image est placée correctement dans le répertoire src
+import './firstshow.css'; // Import du fichier CSS pour d'autres styles si nécessaire
+import backgroundImage from '../../public/img/formation.jpg'; // Assurez-vous que l'image est correctement importée
 import ContactBanner from './contactbanner';
+import CheckIcon from '@mui/icons-material/Check';
 const Firstshow = () => {
-    return (
-        <div>
-        <div className="homepage" style={{ backgroundImage: `url(${backgroundImage})` }}>
-          
-          <main className="main-content">
-            <h1>LA FORMATION EN LIGNE  AVEC WALTER LEARNING</h1>
-            <p className="description">
-              Experts de la formation en ligne , nous vous accompagnons dans le développement de vos compétences professionnelles
-            </p>
-            <ul className="features">
-              <li>Des formations professionnelles 100% en ligne</li>
-              <li>Des formateurs experts spécialistes de leur discipline</li>
-              <li>Suivez votre formation continue où vous voulez, quand vous voulez, à votre rythme</li>
-              <li>Un accompagnement au financement : CPF, Pôle emploi, OPCO, paiement en plusieurs fois</li>
-            </ul>
-            <div className="buttons">
-             <a href="#section-title"> <button className="btn">NOS FORMATIONS</button></a>
-             <div className="header">
-            <ContactBanner />
+  return (
+    <div>
+      <div className="homepage" style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <main className="main-content">
+          <h1 style={{ textAlign: 'left' }}>LA FORMATION EN LIGNE AVEC MATHILDAIA</h1>
+          <p className="description" style={{ textAlign: 'left' }}>
+            Experts de la formation en ligne, nous vous accompagnons dans le développement de vos compétences professionnelles
+          </p>
+          <ul className="features" style={{ textAlign: 'left' }}>
+            <li>{<CheckIcon/>}Des formations professionnelles 100% en ligne</li>
+            <li>{<CheckIcon/>}Des formateurs experts spécialistes de leur discipline</li>
+            <li>{<CheckIcon/>}Suivez votre formation continue où vous voulez, quand vous voulez, à votre rythme</li>
+            <li>{<CheckIcon/>}Un accompagnement au financement : CPF, Pôle emploi, OPCO, paiement en plusieurs fois</li>
+          </ul>
+          <div className="buttons">
+            <a href="#section-title"><button className="btn">NOS FORMATIONS</button></a>
           </div>
-            </div>
-          </main>
+        </main>
+        <div className="header">
+          <ContactBanner />
         </div>
-        </div>
-      );
-
-
-    };
-    
-  
+      </div>
+    </div>
+  );
+};
 
 export default Firstshow;
