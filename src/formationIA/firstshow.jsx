c6b0dcc93b81e886c029c3759945e93f34a8fcce
youssefrayenfehmi <yousseffehmi99@gmail.com>
2024-06-18 12:10:25 +0100

    first

--- src/formationIA/firstshow.jsx
+++ src/formationIA/firstshow.jsx
@@ -9,11 +9,12 @@ const Firstshow = () => {
         <div className="homepage" style={{ backgroundImage: `url(${backgroundImage})` }}>
           
           <main className="main-content">
-            <h1>LA FORMATION EN LIGNE ET PRESENTIELLE AVEC WALTER LEARNING</h1>
+            <h1>LA FORMATION EN LIGNE  AVEC WALTER LEARNING</h1>
             <p className="description">
-              Experts de la formation en ligne et presentielle, nous vous accompagnons dans le développement de vos compétences professionnelles
+              Experts de la formation en ligne , nous vous accompagnons dans le développement de vos compétences professionnelles
             </p>
             <ul className="features">
+              <li>Des formations professionnelles 100% en ligne</li>
               <li>Des formateurs experts spécialistes de leur discipline</li>
               <li>Suivez votre formation continue où vous voulez, quand vous voulez, à votre rythme</li>
               <li>Un accompagnement au financement : CPF, Pôle emploi, OPCO, paiement en plusieurs fois</li>
