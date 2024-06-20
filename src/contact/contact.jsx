a645a0b7d2b933971c56d18c0b1c9ce8251baa7a
youssefrayenfehmi <yousseffehmi99@gmail.com>
2024-06-20 12:22:40 +0100

    first

--- src/contact/contact.jsx
+++ src/contact/contact.jsx
@@ -9,19 +9,16 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-container">
         <div className="footer-section">
-          <h2>MathildaIA</h2>
+          <h2>Mathilda IA</h2>
           <address>
             <p>
-              <FontAwesomeIcon icon={faMapMarkerAlt} /> <a href="https://www.google.com/maps/search/?api=1&query=128+rue+la+Boétie,+Paris,+75008" target="_blank" rel="noopener noreferrer">128 rue la Boétie, Paris, 75008</a>
+              <FontAwesomeIcon icon={faMapMarkerAlt} /> <a href="https://www.google.fr/maps/place/Rue+de+saussure/@48.8873734,2.3146357,17z/data=!3m1!4b1!4m6!3m5!1s0x47e66f0078688c1b:0x173ae88e3b6bc42!8m2!3d48.8873734!4d2.3120608!16s%2Fg%2F11vq3vfx2w?entry=ttu" target="_blank" rel="noopener noreferrer">158 RUE DE SAUSSURE 75017 PARIS</a>
             </p>
             <p>
-              <FontAwesomeIcon icon={faEnvelope} /> <a href="mailto:contact@mister-ia.com">contact@mister-ia.com</a>
+              <FontAwesomeIcon icon={faEnvelope} /> <a href="">contact@Mathilda_IA.com</a>
             </p>
             <p>
-              <FontAwesomeIcon icon={faPhone} /><a href="tel:+33 7 66 14 52 38">+33 7 66 14 52 38</a> 
-            </p>
-            <p>
-              <FontAwesomeIcon icon={faLinkedin} /> <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">Linkedin</a>
+              <FontAwesomeIcon icon={faPhone} /><a href="tel:08 92 97 61 48">08 92 97 61 48</a> 
             </p>
           </address>
         </div>
@@ -36,17 +33,10 @@ const Footer = () => {
         <div className="footer-section">
           <h3>Informations</h3>
           <ul>
-            <a href="pdf/cgv.pdf" target='_blank'><li>CGV</li></a>
-            <a href="pdf/cgv.pdf" target='_blank'><li>Mentions légales</li></a>
-            <a href="pdf/cgv.pdf" target='_blank'><li>Politique de confidentialité</li></a>
-          </ul>
-        </div>
-        <div className="footer-section">
-          <h3>Autres</h3>
-          <ul>
-          <a href="pdf/reglement.pdf" target='_blank'><li>Règlement intérieur</li></a>
-          <a href="pdf/cgv.pdf" target='_blank'><li>Attestation Qualiopi</li></a>
-          <a href="pdf/cgv.pdf" target='_blank'><li>Annuaire personne en situation de handicap</li></a>
+            <a href="pdf/CGV_Mathilda_IA.pdf" target='_blank'><li>CGV</li></a>
+            <a href="pdf/dmention.pdf" target='_blank'><li>Mentions légales</li></a>
+            <a href="pdf/politique.pdf" target='_blank'><li>Politique de confidentialité</li></a>
+            <a href="pdf/reglement.pdf" target='_blank'><li>Règlement intérieur</li></a>
           </ul>
         </div>
       </div>
